145e81fed46f877540e9eaf0315f17a1402bd970
Oliger <Oliger1999@gmail.com>
2023-06-17 14:48:05 +0200

    ndryshim i vogel

--- src/main/java/com/mycompany/MyWebAppApplication.java
+++ src/main/java/com/mycompany/MyWebAppApplication.java
@@ -8,6 +8,8 @@ public class MyWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebAppApplication.class, args);
+
+        System.out.println("Abigela");
     }
 
 }
